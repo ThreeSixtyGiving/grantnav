4c8c2c20a63171b559ff8244407c75033104407b
David Raznick <kindly@gmail.com>
2016-01-12 18:04:13 +0000

    change to grantnav

--- grantnav/wsgi.py
+++ grantnav/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for explore360 project.
+WSGI config for grantnav project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "explore360.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "grantnav.settings")
 
 application = get_wsgi_application()
