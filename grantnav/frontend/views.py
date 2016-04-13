8100ef19506ffab1947ba51203546c0e664f2216
David Raznick <kindly@gmail.com>
2016-04-13 18:02:53 +0100

    [#63] Add location facet
    Use codepoint data to add region/district using postcode and to add
    region to lottery data.

--- grantnav/frontend/views.py
+++ grantnav/frontend/views.py
@@ -16,7 +16,9 @@ BASIC_FILTER = [
     {"bool": {"should": []}},  # Recipient Orgs
     {"bool": {"should": []}},  # Amount Awarded Fixed
     {"bool": {"should": []}},  # Amount Awarded
-    {"bool": {"should": []}}   # Award Year
+    {"bool": {"should": []}},  # Award Year
+    {"bool": {"should": []}},  # recipientRegionName
+    {"bool": {"should": []}}   # recipientDistrictName
 ]
 
 TERM_FILTERS = {
@@ -29,7 +31,9 @@ BASIC_QUERY = {"query": {"bool": {"must":
                   {"query_string": {"query": ""}}, "filter": BASIC_FILTER}},
                "aggs": {
                    "fundingOrganization": {"terms": {"field": "fundingOrganization.id_and_name", "size": 10}},
-                   "recipientOrganization": {"terms": {"field": "recipientOrganization.id_and_name", "size": 10}}}}
+                   "recipientOrganization": {"terms": {"field": "recipientOrganization.id_and_name", "size": 10}},
+                   "recipientRegionName": {"terms": {"field": "recipientRegionName", "size": 20}},
+                   "recipientDistrictName": {"terms": {"field": "recipientDistrictName", "size": 10}}}}
 SIZE = 10
 
 FIXED_AMOUNT_RANGES = [
@@ -365,6 +369,9 @@ def search(request):
         get_clear_all(request, context, json_query)
         for filter_name, index in TERM_FILTERS.items():
             get_terms_facets(request, context, json_query, filter_name + ".id_and_name", filter_name, index)
+
+        get_terms_facets(request, context, json_query, "recipientRegionName", "recipientRegionName", 5)
+        get_terms_facets(request, context, json_query, "recipientDistrictName", "recipientDistrictName", 6)
 
         get_amount_facet(request, context, json_query)
         get_amount_facet_fixed(request, context, json_query)
