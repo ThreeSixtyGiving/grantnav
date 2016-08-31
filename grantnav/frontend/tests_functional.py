3b7bf76fe6178d749443974ef319ccc455447cfe
David Carpenter <caprenter@gmail.com>
2016-08-31 12:58:05 +0100

    [#280] Adds a disclaimer to the funder pages
    Also adds a test

--- grantnav/frontend/tests_functional.py
+++ grantnav/frontend/tests_functional.py
@@ -153,3 +153,10 @@ def test_right_align_amounts_in_other_tables(dataload, server_url, browser, path
 def test_datasets_page(server_url, browser):
     browser.get(server_url + '/datasets')
     assert 'Data used in GrantNav' in browser.find_element_by_tag_name('h1').text
+
+
+def test_disclaimer_on_funder_page(server_url, browser):
+    browser.get(server_url + '/funder/GB-CHC-327114')
+    assert 'This data is provided for information purposes only.' in browser.find_element_by_id('disclaimer').text
+    assert 'Please refer to the funder website for details of current grant programmes, application guidelines and eligibility criteria.' in browser.find_element_by_id('disclaimer').text
+
