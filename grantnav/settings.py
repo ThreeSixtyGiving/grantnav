8e9d694a01a795d7e657f1ee5bafcc622e27b7a1
Michael Wood <michael.wood@opendataservices.coop>
2020-01-20 16:59:25 +0000

    Port of updates commit 13aa42705f6630c587272eda4711fb1cc972b8ea
     - Update instructions in README for EL7 and Distro
     - Update chromedriver to use LATEST
     - Fix Django settings to use MIDDLEWARE instead of deprecated
    MIDDLEWARE_CLASSES (pytest-django errors on this)
     - Update requirements

--- grantnav/settings.py
+++ grantnav/settings.py
@@ -78,7 +78,7 @@ INSTALLED_APPS = (
     'favicon',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
