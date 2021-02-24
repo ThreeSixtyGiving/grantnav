e8c639f86529eb49ab932f3e0a94e3bb1a173ff1
Jared Parnell <jared.parnell@opendataservices.coop>
2021-02-24 17:38:39 +0000

    frontend: context_processors: Add ids to nav

--- grantnav/frontend/context_processors.py
+++ grantnav/frontend/context_processors.py
@@ -6,7 +6,7 @@ def piwik(request):
 
 def navigation(request):
     sections = [
-        {"name": "Get Help", "url": "/help"}, 
-        {"name": "Forum", "url": "https://forum.threesixtygiving.org/c/grantnav/8"}
+        {"name": "Get Help", "url": "/help", "id": "help_link"}, 
+        {"name": "Forum", "url": "https://forum.threesixtygiving.org/c/grantnav/8", "id": "forum_link"}
     ]
     return {'nav_menu': sections}
