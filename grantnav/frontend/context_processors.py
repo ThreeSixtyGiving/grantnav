2d01e0b1f8c58fe06a4023c984dab54ee0faa828
Jared Parnell <jared.parnell@opendataservices.coop>
2021-03-11 10:38:20 +0000

    frontend: nav: Update links
    Add a link to About the data and update the link for Help

--- grantnav/frontend/context_processors.py
+++ grantnav/frontend/context_processors.py
@@ -7,7 +7,8 @@ def piwik(request):
 
 def navigation(request):
     sections = [
-        {"name": "Get Help", "url": "/help", "id": "help_link"},
-        {"name": "Forum", "url": "https://forum.threesixtygiving.org/c/grantnav/8", "id": "forum_link"}
+        {"name": "About the data", "url": "http://grantnav-dev.default.threesixtygiving.uk0.bigv.io/about", "id": "about_link"},
+        {"name": "Forum", "url": "https://forum.threesixtygiving.org/c/grantnav/8", "id": "forum_link"},
+        {"name": "Get Help", "url": "https://help.grantnav.threesixtygiving.org/", "id": "help_link"},
     ]
     return {'nav_menu': sections}
