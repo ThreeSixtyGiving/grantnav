ThreeSixtyGiving/grantnav

commit 916d5f03db5ebf252d0956d357f2e31adb33121e

Filter: Add date range filter.
Adds an option for filtering by date range using a datepicker library.

https://github.com/ThreeSixtyGiving/grantnav/issues/215

grantnav/frontend/views.py
@@ -29,7 +29,8 @@ BASIC_FILTER = [
     {"bool": {"should": []}},  # additional_data.recipientRegionName
     {"bool": {"should": []}},  # additional_data.recipientDistrictName
     {"bool": {"should": []}},  # currency
-    {"bool": {"should": []}}   # additional_data.TSGFundingOrgType
+    {"bool": {"should": []}},   # additional_data.TSGFundingOrgType
+    {"bool": {"should": {"range": {"awardDate": {}}}, "must": {}, "minimum_should_match": 1}},   # Date range
 ]
 
 TermFacet = collections.namedtuple('TermFacet', 'field_name param_name filter_index display_name is_json')
@@ -386,9 +387,27 @@ def get_date_facets(request, context, json_query):
         if bucket.get("selected"):
             context["selected_facets"]["Award Year"].append({"url": bucket["url"], "display_value": value})
 
-    if current_filter:
+    input_range = json_query["query"]["bool"]["filter"][9]["bool"]["should"]["range"]["awardDate"]
+
+    if current_filter or input_range:
         json_query["query"]["bool"]["filter"][4]["bool"]["should"] = []
+        json_query["query"]["bool"]["filter"][9]["bool"]["should"]["range"]["awardDate"] = {}
         results['aggregations']["awardYear"]['clear_url'] = request.path + '?' + create_parameters_from_json_query(json_query)
+
+    if input_range:
+        new_json_query = copy.deepcopy(json_query)
+        new_json_query["query"]["bool"]["filter"][4]["bool"]["should"] = []
+        new_json_query["query"]["bool"]["filter"][9]["bool"]["should"]["range"]["awardDate"] = {}
+
+        lt, gte = input_range.get('lt'), input_range.get('gte')
+        display_value = ''
+        if gte:
+            display_value += f'From: {utils.date_to_yearmonth(gte)} '
+        if lt:
+            display_value += f'To: {utils.date_to_yearmonth(lt, max=True)}'
+
+        context["selected_facets"]["Award Date"].append({"url": request.path + '?' + create_parameters_from_json_query(new_json_query), "display_value": display_value})
+
     main_results['aggregations']["awardYear"] = results['aggregations']["awardYear"]
 
 
@@ -604,6 +623,15 @@ def create_json_query_from_parameters(request):
         amount_filter['lte'] = max_amount
     json_query["query"]["bool"]["filter"][3]["bool"]["should"]["range"]["amountAwarded"] = amount_filter
 
+    date_filter = {}
+    min_date = utils.yearmonth_to_date(request.GET.get('min_date', ''))
+    if min_date:
+        date_filter['gte'] = min_date
+    max_date = utils.yearmonth_to_date(request.GET.get('max_date', ''), True)
+    if max_date:
+        date_filter['lt'] = max_date
+    json_query["query"]["bool"]["filter"][9]["bool"]["should"]["range"]["awardDate"] = date_filter
+
     for term_facet in TERM_FACETS:
         term_facet_from_parameters(request, json_query, term_facet.field_name, term_facet.param_name,
                                    term_facet.filter_index, term_facet.display_name, term_facet.is_json)
@@ -688,6 +716,13 @@ def create_parameters_from_json_query(json_query, **extra_parameters):
     max_amount = json_query["query"]["bool"]["filter"][3]["bool"]["should"]["range"]["amountAwarded"].get('lte')
     if max_amount:
         parameters['max_amount'] = [str(max_amount)]
+
+    min_date = json_query["query"]["bool"]["filter"][9]["bool"]["should"]["range"]["awardDate"].get('gte')
+    if min_date:
+        parameters['min_date'] = [utils.date_to_yearmonth(min_date)]
+    max_date = json_query["query"]["bool"]["filter"][9]["bool"]["should"]["range"]["awardDate"].get('lt')
+    if max_date:
+        parameters['max_date'] = [utils.date_to_yearmonth(max_date, True)]
 
     for term_facet in TERM_FACETS:
         term_parameters_from_json_query(parameters, json_query, term_facet.field_name, term_facet.param_name,
@@ -863,6 +898,23 @@ def search(request):
                     pass
             json_query["query"]["bool"]["filter"][3]["bool"]["should"]["range"]["amountAwarded"] = new_filter
             json_query["query"]["bool"]["filter"][3]["bool"]["must"] = {"term": {"currency": current_currency}}
+            return redirect(request.path + '?' + create_parameters_from_json_query(json_query))
+
+        min_date = utils.yearmonth_to_date(request.GET.get('new_min_date', ''))
+        max_date = utils.yearmonth_to_date(request.GET.get('new_max_date', ''), True)
+        if min_date or max_date:
+            new_filter = {}
+            if min_date:
+                try:
+                    new_filter['gte'] = min_date
+                except ValueError:
+                    pass
+            if max_date:
+                try:
+                    new_filter['lt'] = max_date
+                except ValueError:
+                    pass
+            json_query["query"]["bool"]["filter"][9]["bool"]["should"]["range"]["awardDate"] = new_filter
             return redirect(request.path + '?' + create_parameters_from_json_query(json_query))
 
         context['selected_facets'] = collections.defaultdict(list)

grantnav/frontend/sass/custom.scss
@@ -84,6 +84,20 @@ dd {
   }
 }
 
+// Datepicker Custom Styles
+
+.datepicker table tr td span.active.active,
+.datepicker table tr td span.active:active,
+.datepicker table tr td span.active:hover.active,
+.datepicker table tr td span.active.active.focus,
+.datepicker table tr td span.active.active:focus,
+.datepicker table tr td span.active.active:hover,
+.datepicker table tr td span.active:hover:active,
+.datepicker table tr td span.active:hover.active:hover{
+  color: #000;
+  background-color: #d3eaed;
+}
+
 // From main.css
 
 /* ==========================================================================

grantnav/frontend/static/css/theme.css
@@ -2747,6 +2747,17 @@ dd {
     .dl-horizontal dd,
     .top-stats dd {
       margin-left: 180px; } }
+.datepicker table tr td span.active.active,
+.datepicker table tr td span.active:active,
+.datepicker table tr td span.active.active:hover,
+.datepicker table tr td span.active.active.focus,
+.datepicker table tr td span.active.active:focus,
+.datepicker table tr td span.active.active:hover,
+.datepicker table tr td span.active:hover:active,
+.datepicker table tr td span.active.active:hover:hover {
+  color: #000;
+  background-color: #d3eaed; }
+
 /* ==========================================================================
    Author's custom styles
    ========================================================================== */

grantnav/frontend/templates/components/filters/lists/filter-list-year.html
@@ -1,14 +1,38 @@
-{% load get_amount from frontend %}
+{% load frontend %}
+
 <div class="filter-list margin-bottom:2">
   <details class="filter-list__accordion" open>
     <summary class="filter-list__label">
-      <div>Award Year {% if results.aggregations.awardYear.clear_url %} <a href="{{results.aggregations.awardYear.clear_url}}"> <small>(clear)</small></a> {% endif%}</div>
+      <div>Award Date {% if results.aggregations.awardYear.clear_url %} <a href="{{results.aggregations.awardYear.clear_url}}"> <small>(clear)</small></a> {% endif%}</div>
       {% include 'tokens/accordion-toggle-icon.html' %}
     </summary>
     <div class="filter-list__contents-wrapper">
+      {% block filterForm %}
+        <form class="filter-list__contents--form">
+
+          <span class="input-daterange input-group" id="datepicker">
+            <div class="filter-list__contents--form-item">
+              <label for="min-month">From Month</label>
+              <input id="min-month" name="new_min_date" class="form-control" placeholder="mm/yyyy" value="{{query.query.bool.filter.9.bool.should.range.awardDate.gte | min_yearmonth}}"/>
+            </div>
+
+            <div class="filter-list__contents--form-item">
+              <label for="max-month">To Month</label>
+              <input id="max-month" name="new_max_date" placeholder="mm/yyyy" class="form-control input-xs" value="{{query.query.bool.filter.9.bool.should.range.awardDate.lt | max_yearmonth}}"/>
+            </div>
+          </span>
+
+
+          <input name="json_query" class="form-control" type="hidden" value="{{json_query}}">
+          <button type="submit" class="filter-list__contents--form-submit">Apply</button>
+
+        </form>
+      {% endblock filterForm %}
+
       {% block filterOption %}
       {% endblock filterOption %}
       <ul class="filter-list__listing">
+         {% if not query.query.bool.filter.9.bool.should.range.awardDate %}
          {% for bucket in results.aggregations.awardYear.buckets %}
             {% if forloop.counter <= query.extra_context.awardYear_facet_size %}          <li>
             <a class="filter-list__filter-item {% if bucket.selected %} active{% endif %}" href="{{bucket.url}}">
@@ -20,6 +44,7 @@
           </li>
           {% endif %}
         {% endfor %}
+        {% endif %}
       </ul>
     </div>
   </details>

grantnav/frontend/templates/search.html
@@ -8,6 +8,7 @@
 
 {% block main_content %}
   <main class="layout__content">
+
     <div class="grantnav-search__wrapper">
       <div class="grantnav-search__sidebar">
         <h2 class="grantnav-search__sidebar--heading">Refine your search</h2>
@@ -102,7 +103,15 @@
   </main>
 {% endblock %}
 
+
+{% block extra_style %}
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker3.standalone.min.css" rel="stylesheet">
+
+{% endblock %}
 {% block extra_scripts %}
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
 
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/URI.js/1.18.1/URI.min.js"></script>
 <script>
@@ -166,6 +175,19 @@ $(function() {
       $('#' + param + '-button').hide()
     }
   })
+
+
+  $('#datepicker').datepicker({
+    format: "mm/yyyy",
+    startDate: "01/1990",
+    endDate: "{% now "12/Y" %}",
+    startView: 2,
+    minViewMode: 1,
+    maxViewMode: 2,
+    autoclose: true,
+    defaultViewDate: '01/2010'
+  });
 });
+
 </script>
 {% endblock %}

grantnav/frontend/templatetags/frontend.py
@@ -184,3 +184,13 @@ def get_dataset(grant):
 def get_current_sort(query):
     for key, value in query['sort'].items():
         return key + " " + value["order"]
+
+
+@register.filter(name='min_yearmonth')
+def min_yearmonth(date):
+    return utils.date_to_yearmonth(date)
+
+
+@register.filter(name='max_yearmonth')
+def max_yearmonth(date):
+    return utils.date_to_yearmonth(date, True)

grantnav/utils.py
@@ -11,3 +11,41 @@ def currency_prefix(currency):
         return '£'
     if currency:
         return currency + ' '
+
+
+def yearmonth_to_date(yearmonth, max=False):
+    ''' Convert YYYY/MM into YYYY-MM-DD, yeardate is from url so has to be checked'''
+    date_split = yearmonth.split('/')
+    if len(date_split) != 2:
+        return
+    month, year = date_split
+    try:
+        month, year = int(month), int(year)
+        # need first day of next month
+        if max:
+            if month == 12:
+                year += 1
+                month = 1
+            else:
+                month += 1
+    except ValueError:
+        return
+    return f'{year}-{month:02}-01'
+
+
+def date_to_yearmonth(date, max=False):
+    ''' Convert YYYY-MM-DD into YYYY/MM, date is from internal representations so safe'''
+    if not date:
+        return ''
+    date_split = [int(part) for part in date.split('-')]
+    year, month, day = date_split
+
+    if max:
+        # need first day of previous month
+        if month == 1:
+            year -= 1
+            month = 12
+        else:
+            month -= 1
+
+    return f'{month:02}/{year}'

commit 3b75bc89fa7c6c56309dba304e977a98d8ebdf3f

frontend: views: Don't crash if old URL used without date range
Commit 916d5f03db5ebf252d0956d357f2e31adb33121e introduced new date
range filter and code to process it in
create_parameters_from_json_query

However old URL's can still exist that pass the full JSON and don't
have that set, so we much make sure old URL's work by only processing
it in create_parameters_from_json_query if it is in the JSON

grantnav/frontend/views.py
@@ -723,12 +723,14 @@ def create_parameters_from_json_query(json_query, **extra_parameters):
     if max_amount:
         parameters['max_amount'] = [str(max_amount)]
 
-    min_date = json_query["query"]["bool"]["filter"][9]["bool"]["should"]["range"]["awardDate"].get('gte')
-    if min_date:
-        parameters['min_date'] = [utils.date_to_yearmonth(min_date)]
-    max_date = json_query["query"]["bool"]["filter"][9]["bool"]["should"]["range"]["awardDate"].get('lt')
-    if max_date:
-        parameters['max_date'] = [utils.date_to_yearmonth(max_date, True)]
+    # Date range filter was only added recently and old URL's will not have it in the JSON. So check it is there first:
+    if len(json_query["query"]["bool"]["filter"]) >= 10:
+        min_date = json_query["query"]["bool"]["filter"][9]["bool"]["should"]["range"]["awardDate"].get('gte')
+        if min_date:
+            parameters['min_date'] = [utils.date_to_yearmonth(min_date)]
+        max_date = json_query["query"]["bool"]["filter"][9]["bool"]["should"]["range"]["awardDate"].get('lt')
+        if max_date:
+            parameters['max_date'] = [utils.date_to_yearmonth(max_date, True)]
 
     for term_facet in TERM_FACETS:
         term_parameters_from_json_query(parameters, json_query, term_facet.field_name, term_facet.param_name,