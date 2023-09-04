e0d2428531e7c3792b04422441f1beed908ea670
Michael Wood <michael.wood@opendataservices.coop>
2023-09-04 12:19:32 +0100

    settings: Namespace the static directory
    This avoids any potential namespace clashes on servers which have other
    applications running on them as this can be an URL setup in the global
    webserver name space.

--- grantnav/settings.py
+++ grantnav/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/grantnav_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Logging
