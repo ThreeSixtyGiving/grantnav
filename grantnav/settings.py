bbc66208a8a171d94889afbef5f75b456b76aeff
Bibiana Cristofol Amat <bcristofol@gmail.com>
2025-03-05 12:41:27 +0100

    dealer: remove from settings and requirements

--- grantnav/settings.py
+++ grantnav/settings.py
@@ -96,7 +96,6 @@ MIDDLEWARE = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'dealer.contrib.django.Middleware',
 )
 
 if DEBUG:
