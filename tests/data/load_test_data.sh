8906dc71a1afbb0c35db8848d9e498bbd907ed1b
Michael Wood <michael.wood@opendataservices.coop>
2025-03-14 12:30:40 +0000

    tests: Rewrite and improve all existing tests
    Refactor and improve tests:
    * Upgrade to new selenium API (by_id -> By.ID)
    * reverse_lazy in tests instead of string concat
    * TestCase derived tests for better django compatibility and reducing
      the software stack/moving parts
    * Improve timing for more reliable browser results
    * Add Re-usable components for browser tests
    * Split link checker from browser tests
    * Remove tests which don't test anything
    * Change json lines files to have correct file extension
    * Update github workflow and split into new test links for parallel
      running

---
+++ tests/data/load_test_data.sh
@@ -0,0 +1,1 @@
+./dataload/import_to_elasticsearch.py ./dataload/test_data/a002400000KeYdsAAF-currency.json ./dataload/test_data/a002400000OiDBQAA3.json ./dataload/test_data/awefw001p00000zgyHZAAY.json  --clean 
