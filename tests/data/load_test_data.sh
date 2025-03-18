a1d37c180118bbde048abb13be8c7d9bdfb8578b
Michael Wood <michael.wood@opendataservices.coop>
2025-03-18 14:36:54 +0000

    tests: Remove obsolete load_test_data script
    This may be misleading as it only loads some of the test data that was
    for a specific purpose.

--- tests/data/load_test_data.sh
+++
@@ -1,1 +0,0 @@
-./dataload/import_to_elasticsearch.py ./dataload/test_data/a002400000KeYdsAAF-currency.json ./dataload/test_data/a002400000OiDBQAA3.json ./dataload/test_data/awefw001p00000zgyHZAAY.json  --clean 
