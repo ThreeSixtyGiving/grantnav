dfefae79d3d2a3403f496f5ff042b0a56fc3857b
Michael Wood <michael.wood@opendataservices.coop>
2025-04-10 16:53:00 +0100

    api: handlers: Don't check path before applying CORS
    There are strange but valid cases where you may do //api/etc or the name
    space changes from the URLs py and not updated here so remove this.

--- grantnav/api/handlers.py
+++ grantnav/api/handlers.py
@@ -2,10 +2,9 @@ from corsheaders.signals import check_request_enabled
 
 
 def cors_allow_api_to_all_360(sender, request, **kwargs):
-    if request.path.startswith("/api/"):
-        if "Origin" in request.headers:
-            if request.headers["Origin"].endswith(".threesixtygiving.org"):
-                return True
+    if "Origin" in request.headers:
+        if request.headers["Origin"].endswith(".threesixtygiving.org"):
+            return True
     return False
 
 
